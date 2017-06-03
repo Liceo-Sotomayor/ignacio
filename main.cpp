2625196ca46b85bc3b8d13a0b015f75f7937949d
NACH021 <ignaciomelly@gmail.com>
2017-06-03 12:03:18 -0400

    mi primer commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    float ancho;
+    float alto;
+    float area;
+    cout << "ingrese el alto " << endl;
+    cin >> alto;
+    cout << "ingrese el ancho" << endl;
+    cin >> ancho;
+    area = alto * ancho;
+    cout 
+
+}
